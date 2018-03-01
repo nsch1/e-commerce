c98040acd93dc2a200f5c2c96e330f0c0b66c557
nsch1 <nick.schooneman1@gmail.com>
2018-03-01 14:41:42 +0100

    Add route for all products

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,16 @@ const app = express()
 app.use(cors())
 
 app.listen(4001, () => console.log('Express API listening on port 4001'))
+
+app.get('/products', (req, res) => {
+  Product.findAll({
+    attributes: ['id', 'name', 'price']
+  })
+    .then(result => {
+      if (!result) return res.status(404).json({ message: "Sorry no products found."})
+      res.json(result)
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Oops something went wrong! Try again!"})
+    })
+})
