8940e5988108204e3caaac08ca19b25685ac4952
nsch1 <nick.schooneman1@gmail.com>
2018-03-01 17:16:48 +0100

    Add API

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const db = require('./models')
 const { Product } = db
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.listen(4001, () => console.log('Express API listening on port 4001'))
 
@@ -21,6 +23,16 @@ app.get('/products', (req, res) => {
     })
 })
 
+app.post('/products', (req, res) => {
+  const product = req.body
+  console.log(product)
+
+  Product.create(product)
+    .then(entity => {
+      res.status(201).send(entity)
+    })
+})
+
 app.get('/products/:id', (req, res) => {
   Product.findById(req.params.id)
     .then(result => {
@@ -30,4 +42,42 @@ app.get('/products/:id', (req, res) => {
     .catch(err => {
       res.status(500).json({ message: "Oops something went wrong! Try again!" })
     })
+})
+
+app.put('/products/:id', (req, res) => {
+  const productId = Number(req.params.id)
+  const updates = req.body
+
+  Product.findById(req.params.id)
+    .then(entity => {
+      return entity.update(updates)
+    })
+    .then(final => {
+      res.send(final)
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+
+})
+
+app.delete('/products/:id', (req, res) => {
+  Product.findById(req.params.id)
+  .then(entity => {
+    return entity.destroy()
+  })
+  .then(_ => {
+    res.send({
+      message: 'The product was deleted succesfully'
+    })
+  })
+  .catch(error => {
+    res.status(500).send({
+      message: `Something went wrong`,
+      error
+    })
+  })
 })
