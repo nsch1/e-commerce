6f6d5c7149cd196fb2841be0c589e2e5deb57139
nsch1 <nick.schooneman1@gmail.com>
2018-03-02 12:12:12 +0100

    Add User model & migration

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ const db = require('./models')
 const { Product } = db
 const app = express()
 const productsRouter = require('./products/router')
+const usersRouter = require('./users/router')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(productsRouter)
+app.use(usersRouter)
 
 app.listen(process.env.PORT || 4001, () => console.log('Express API listening on port 4001'))
