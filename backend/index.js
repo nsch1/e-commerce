02d22f5879b9bcc5d846f1f18867e5d1bcc57494
nsch1 <nick.schooneman1@gmail.com>
2018-03-02 14:13:27 +0100

    Forgot to commit in a while. Add authentication stuff

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,45 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const db = require('./models')
-const { Product } = db
+const { User } = db
+const verify = require('./jwt').verify
+const jwt = require('jsonwebtoken')
 const app = express()
 const productsRouter = require('./products/router')
 const usersRouter = require('./users/router')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(function (req, res, next) {
+  if (!req.headers.authorization) return next()
+
+  const auth = req.headers.authorization.split(' ')
+  if (auth[0] === 'Bearer') {
+    verify(auth[1], function (err, jwt) {
+      if (err) {
+        console.error(err)
+        res.status(400).send({
+          message: "JWT token invalid"
+        })
+      }
+      else {
+        User
+          .findById(jwt.data.id)
+          .then(entity => {
+            req.user = entity
+            next()
+          })
+          .catch(err => {
+            console.error(err)
+            res.status(500).send({
+              message: 'Something went horribly wrong'
+            })
+          })
+      }
+    })
+  }
+  else next()
+})
 app.use(productsRouter)
 app.use(usersRouter)
 
